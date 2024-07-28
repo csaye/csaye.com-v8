27c60f435adc0fd255aa3558e3cf779d75a5f38c
Cooper Saye <coopersaye@gmail.com>
2024-07-28 02:01:05 -0400

    Fonts and icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,61 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import localFont from 'next/font/local'
 import './globals.scss'
 
-const inter = Inter({ subsets: ['latin'] })
+const satoshi = localFont({
+  src: [
+    {
+      path: '../public/fonts/Satoshi/Satoshi-Light.otf',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Satoshi/Satoshi-LightItalic.otf',
+      weight: '300',
+      style: 'italic',
+    },
+    {
+      path: '../public/fonts/Satoshi/Satoshi-Regular.otf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Satoshi/Satoshi-Italic.otf',
+      weight: '400',
+      style: 'italic',
+    },
+    {
+      path: '../public/fonts/Satoshi/Satoshi-Medium.otf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Satoshi/Satoshi-MediumItalic.otf',
+      weight: '500',
+      style: 'italic',
+    },
+    {
+      path: '../public/fonts/Satoshi/Satoshi-Bold.otf',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Satoshi/Satoshi-BoldItalic.otf',
+      weight: '700',
+      style: 'italic',
+    },
+    {
+      path: '../public/fonts/Satoshi/Satoshi-Black.otf',
+      weight: '900',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Satoshi/Satoshi-BlackItalic.otf',
+      weight: '900',
+      style: 'italic',
+    },
+  ],
+})
 
 export const metadata: Metadata = {
   title: 'Cooper Saye',
@@ -15,7 +68,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body className={satoshi.className}>{children}</body>
     </html>
   )
 }
